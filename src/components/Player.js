8754a29e42aa9350dd4075e76c0a811bdc4dea01
DaoAnhTu2105 <tudase151149@gmail.com>
2023-05-19 16:02:36 +0700

    using map in Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,69 +1,22 @@
 import React from "react";
+import { players } from "../shared/ListOfPlayers";
 
 const Player = () => {
   return (
     <div className="main-container">
       <div className="grid-container">
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/cr.jpg" alt="" />
-            <h3>Cristiano Ronaldo</h3>
-            <p className="title">Manchester United</p>
-            <p>
-              <button> Detail</button>
-            </p>
+        {players.map((p) => (
+          <div className="grid-item">
+            <div className="card">
+              <img src={p.img} alt="" />
+              <h3>{p.name}</h3>
+              <p className="title">{p.club}</p>
+              <p>
+                <button> Detail</button>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/kante.jpg" alt=""></img>
-            <h3>Kante</h3>
-            <p className="title">Chealsea</p>
-            <p>
-              <button> Detail</button>
-            </p>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/messi.jpg" alt=""></img>
-            <h3>Messi</h3>
-            <p className="title">PSG</p>
-            <p>
-              <button> Detail</button>
-            </p>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/neymar.jpg" alt=""></img>
-            <h3>Neymar</h3>
-            <p className="title">PSG</p>
-            <p>
-              <button> Detail</button>
-            </p>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/kane.jpg" alt=""></img>
-            <h3>Kane</h3>
-            <p className="title">Tottemham</p>
-            <p>
-              <button> Detail</button>
-            </p>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="card">
-            <img src="assets/images/haaland.jpg" alt=""></img>
-            <h3>Haaland</h3>
-            <p className="title">Manchester City</p>
-            <p>
-              <button> Detail</button>
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
